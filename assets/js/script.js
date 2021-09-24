cc36c11a2fcfc110d03f8db7932ec56b4d5a8189
Peter <warrenp11@gmail.com>
2021-09-23 23:36:55 -0400

    clean up final code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,16 @@
 var todayDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").html(todayDate);
 
-
 function timeTracker() {
-    //get current number of hours.
+    //get current time in hours
     var timeNow = moment().hour();
 
     // loop over time blocks
     $(".time-block").each(function () {
+        // create var which holds a single time block
         var timeBlock = parseInt($(this).attr("id").split("hour")[1]); 
 
-        /* *****************************************************************
-        // $(".time-block") *** targets each row with a class of time-block
-        // .each *** applies to each (".time-block") in doc
-        // 'var timeBlock' *** creates a variable to store info 
-        // 'parseInt()' *** takes entire argument, makes it an interger 
-        // $(this).attr("id") *** targets div with class time-block, finds attribute with id (ie. 'hour8')
-        // '.split("hour")' *** splits the id between hour and # (ex. 'hour', '8')
-        // [1] *** takes the second part of the split to targert each timeblock (ex. 8)
-        ***************************************************************** */
-
-        // To check the time and add the classes for background indicators
+        // check the time and add the classes for background indicators
         if (timeBlock < timeNow) {
             $(this).removeClass("future");
             $(this).removeClass("present");
@@ -40,16 +30,16 @@ function timeTracker() {
     });
 };
 
+// click event saveBtn
 $(".saveBtn").on("click", function () {
 
-    // Get nearby values of reach row's time and text
+    // create variables to store a row's time and text
     var time = $(this).parent().attr("id");
     var text = $(this).siblings(".description").val();
 
     // Save text in local storage
     localStorage.setItem(time, text);
 });
-
 
 // Get item from local storage if any
 $("#hour8 .description").val(localStorage.getItem("hour8"));
