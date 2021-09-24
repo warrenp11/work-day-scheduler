152e87d98622d0e9bdc788a7e8bd364c0cd6b3ce
Peter <warrenp11@gmail.com>
2021-09-23 21:54:50 -0400

    add ability to load previously saved tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,8 +37,8 @@ function timeTracker() {
             $(this).removeClass("past");
             $(this).addClass("future");
         }
-    })
-}
+    });
+};
 
 $(".saveBtn").on("click", function () {
 
@@ -49,5 +49,19 @@ $(".saveBtn").on("click", function () {
     // Save text in local storage
     localStorage.setItem(time, text);
 });
+
+
+// Get item from local storage if any
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#hour18 .description").val(localStorage.getItem("hour18"));
 
 timeTracker();
