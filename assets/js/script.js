d4b0f41b57e992f91f0c74074b1d5a5e5cffc6cd
Peter <warrenp11@gmail.com>
2021-09-23 20:30:01 -0400

    add save function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,17 @@ function timeTracker() {
 
     // loop over time blocks
     $(".time-block").each(function () {
-        var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+        var timeBlock = parseInt($(this).attr("id").split("hour")[1]); 
+
+        /* *****************************************************************
+        // $(".time-block") *** targets each row with a class of time-block
+        // .each *** applies to each (".time-block") in doc
+        // 'var timeBlock' *** creates a variable to store info 
+        // 'parseInt()' *** takes entire argument, makes it an interger 
+        // $(this).attr("id") *** targets div with class time-block, finds attribute with id (ie. 'hour8')
+        // '.split("hour")' *** splits the id between hour and # (ex. 'hour', '8')
+        // [1] *** takes the second part of the split to targert each timeblock (ex. 8)
+        ***************************************************************** */
 
         // To check the time and add the classes for background indicators
         if (timeBlock < timeNow) {
@@ -26,9 +36,18 @@ function timeTracker() {
             $(this).removeClass("present");
             $(this).removeClass("past");
             $(this).addClass("future");
-
         }
     })
 }
+
+$(".saveBtn").on("click", function () {
+
+    // Get nearby values of reach row's time and text
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+
+    // Save text in local storage
+    localStorage.setItem(time, text);
+});
 
 timeTracker();
